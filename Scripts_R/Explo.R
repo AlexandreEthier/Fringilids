48190e1deb4f4d4e8fcfaa6d90ae596b143eec49
AEthier-AI <alex.ethie@gmail.com>
2026-02-12 16:29:18 -0500

    Graphique d'abondance standardisée pour les 4 espèces

--- Scripts_R/Explo.R
+++ Scripts_R/Explo.R
@@ -5,6 +5,7 @@ setwd("C:/Users/alexe/Fringilids")
 library(readxl)
 library(dplyr)
 library(ggplot2)
+library(tidyr)
 
 
 
@@ -25,52 +26,69 @@ abond <- read_excel("C:/Users/alexe/Fringilids/Data/Abondance.xlsx") %>% # Stand
          TAPI = "Tarin des pins")
 
 str(abond)
-
 abond$Annee <- as.factor(abond$Annee)
 
-# Visualisation graphique
+# Si on veut "tidyr" le dataframe (C'est-à-dire le mettre en une seule colonne)
 
 abond <- abond %>% 
-  group_by(Annee) %>% 
-  mutate(Ntot = sum(c(DUSA, JABO, SIFL, TAPI))) %>% 
-  ungroup()
+  gather(abond, key = "Espece", DUSA:TAPI)  # Fct. du package "tidyr"
 
-plot_ntot <- ggplot(abond, aes(x = Annee, y = Ntot))+
+abond # Dataframe en une seule colonne
+      # 120 observations (1 abondance/année x 30 ans x 4 espèces)
+
+abond <- abond %>% 
+  mutate(abond_std = abond/Effort) # Ajout de l'effort standardisé
+
+abond$abond_std <- as.numeric(abond$abond_std)
+abond$Espece <- as.factor(abond$Espece)
+
+str(abond)
+
+# Visualisation graphique de l'abondance des 4 espèces en fonction des années
+
+plot_ab <- ggplot(abond, aes(x = Annee, y = abond, group = Espece, color = Espece))+
   geom_point()+
-  geom_line(stat = "identity", col = "red")
-  geom_text(aes(label = Effort),
-            vjust = -1,
-            colour = "black")+
-  labs(title = "Abondance totale des espèces cibles par année",
+  geom_line()+
+  labs(title = "Abondance des espèces cibles par année",
        x = "Année",
-       y = "N. individus recensés")+
-  scale_y_continuous(limits = c(0, 300000),
-                     breaks = seq(0, 300000, 50000))+
+       y = "N. d'oiseaux",
+       color = "Espèce")+
   theme_classic()
-plot_ntot
+plot_ab
+
+
+# Visualisation graphique de l'abondance STANDARDISÉE des 4 espèces en fonction des années
+
+plot_std <- ggplot(abond, aes(x = Annee, y = abond_std, group = Espece, color = Espece))+
+  geom_point(size = 3)+
+  geom_line(linewidth = 1.5)+
+  scale_y_continuous(limits = c(0, 600), n.breaks = 15)+
+  labs(title = "Abondance standardisée des espèces cibles par année",
+       x = "Année",
+       y = "N. d'oiseaux/h",
+       color = "Espèce")+
+  theme(axis.line.x = element_line(color = "black", linewidth = 0.5),
+        axis.line.y = element_line(color = "black", linewidth = 0.5),
+        panel.background = element_blank())
+plot_std
+
+
+# Graphique avec l'effort standardisé donne exactement la même chose
+
 
 # DUSA
 
 plot_dusa_tot <- ggplot(abond, aes(x = Annee, y = DUSA))+
   geom_point()+
+  geom_line()+
   labs(title = "Abondance du DUSA par année",
        x = "Année",
        y = "N. individus recensés")+
   theme_classic()
 plot_dusa_tot
 
+xtabs(Effort ~ Annee, data = abond)
 
-# BAGUAGE ####
-
-# Chargement des données
-
-bag <- read_excel("C:/Users/alexe/Fringilids/Data/Baguage.xlsx") %>% 
-  rename(Prefixe = "Préfixe",
-         Espece = "Espèce",
-         Abreviation = "Espèce (abréviation)",
-         Age = "Âge",
-         Annee = "Année",
-         Municipalite = "Municipalité")
 
 
 
