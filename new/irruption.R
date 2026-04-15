a153c6da9f4b0d6b0dd2843bae8e6593543e0fb0
AEthier-AI <alex.ethie@gmail.com>
2026-04-15 11:13:08 -0400

    Définition irruptions + représentation graphique pour 4 sp.

--- new/irruption.R
+++ new/irruption.R
@@ -1,7 +1,7 @@
 # Chargements des bibliothèques -------------------------------------------
 
 library(readxl)
-
+library(ggplot2)
 
 
 # Chargement des répertoires de travail -----------------------------------
@@ -24,7 +24,10 @@ JABO <- JABO[JABO$Annee >= "2007",]
 SIFL <- SIFL[SIFL$Annee >= "2007",]
 TAPI <- TAPI[TAPI$Annee >= "2007",]
 
-str(DUSA)
+DUSA$irruption <- as.factor(DUSA$irruption)
+JABO$irruption <- as.factor(JABO$irruption)
+SIFL$irruption <- as.factor(SIFL$irruption)
+TAPI$irruption <- as.factor(TAPI$irruption)
 
 
 # Calcul des irruptions ---------------------------------------------------
@@ -44,27 +47,138 @@ str(DUSA)
 # On prend la valeur la plus petite de D (exemple -2.1) en valeur absolue comme seuil (2.1). Si D d'une année dépasse ce seuil (ex: D = 3.4 en 2016), alors 2016 est une année irruptive. 
 
 
+##### DUSA
+
+# Extraction param. équation
+
+DUSA_Ni <- DUSA$abond_std
+DUSA_P <- mean(DUSA$abond_std)
+
+DUSA_Nipi <- DUSA_Ni - DUSA_P # Numérateur
+
+DUSA_sigma <- sd(DUSA_Nipi) # Sigma
+
+DUSA_Di <- DUSA_Nipi/DUSA_sigma # Standard deviate
+
+DUSA_seuil <- abs(min(DUSA_Di)) # Seuil fixé pour irruption
+
+DUSA_irr <- DUSA_Di > DUSA_seuil # Vecteur logique TRUE or FALSE pour irruption
+
+# Save object to load in data_clean.R
+# save(DUSA_irr, file = "DUSA_irr.R")
+
+# Représentation graphique
+
+DUSA_plot <- ggplot(DUSA, aes(x = Annee, y = abond_std, color = irruption))+
+  scale_color_manual(values = c( "1" = "orange", "0" = "black"))+
+  geom_point(size = 4)+
+  geom_path(linewidth = 1, col = "#a7a7a7")+
+  labs(title = "Abondance standardisée annuelle du Durbec des sapins pour les années irruptives VS non-irruptives",
+       x = "Année",
+       y = expression("N. individus recensés *" ~ heure^{-1}))+
+  geom_hline(yintercept = mean(DUSA$abond_std), col = "red", lty = 2)+
+  theme_classic()+
+  theme(axis.text.x = element_text(size = 10, angle = 45, vjust = 0.8))
+DUSA_plot
 
 
+##### JABO
+
+JABO_Ni <- JABO$abond_std
+JABO_P <- mean(JABO$abond_std)
+
+JABO_Nipi <- JABO_Ni - JABO_P # Numérateur
+
+JABO_sigma <- sd(JABO_Nipi) # Sigma
+
+JABO_Di <- JABO_Nipi/JABO_sigma # Standard deviate
+
+JABO_seuil <- abs(min(JABO_Di)) # Seuil fixé pour irruption
+
+JABO_irr <- JABO_Di > JABO_seuil # Vecteur logique TRUE or FALSE pour irruption
+
+# Save object to load in data_clean.R
+# save(JABO_irr, file = "JABO_irr.R")
+
+# Représentation graphique
+
+JABO_plot <- ggplot(JABO, aes(x = Annee, y = abond_std, color = irruption))+
+  scale_color_manual(values = c("1" = "#009929", "0" = "black"))+
+  geom_point(size = 4)+
+  geom_path(linewidth = 1, col = "#a7a7a7")+
+  labs(title = "Abondance standardisée annuelle du Jaseur boréal pour les années irruptives VS non-irruptives",
+       x = "Année",
+       y = expression("N. individus recensés *" ~ heure^{-1}))+
+  geom_hline(yintercept = mean(JABO$abond_std), col = "red", lty = 2)+
+  theme_classic()+
+  theme(axis.text.x = element_text(size = 10, angle = 45, vjust = 0.8))
+JABO_plot
 
 
+##### SIFL
+
+SIFL_Ni <- SIFL$abond_std
+SIFL_P <- mean(SIFL$abond_std)
+
+SIFL_Nipi <- SIFL_Ni - SIFL_P # Numérateur
+
+SIFL_sigma <- sd(SIFL_Nipi) # Sigma
+
+SIFL_Di <- SIFL_Nipi/SIFL_sigma # Standard deviate
+
+SIFL_seuil <- abs(min(SIFL_Di)) # Seuil fixé pour irruption
+
+SIFL_irr <- SIFL_Di > SIFL_seuil # Vecteur logique TRUE or FALSE pour irruption
+
+# Save object to load in data_clean.R
+# save(SIFL_irr, file = "SIFL_irr.R")
+
+# Représentation graphique
+
+SIFL_plot <- ggplot(SIFL, aes(x = Annee, y = abond_std, color = irruption))+
+  scale_color_manual(values = c("1" = "turquoise", "0" = "black"))+
+  geom_point(size = 4)+
+  geom_path(linewidth = 1, col = "#a7a7a7")+
+  labs(title = "Abondance standardisée annuelle du Sizerin flammé pour les années irruptives VS non-irruptives",
+       x = "Année",
+       y = expression("N. individus recensés *" ~ heure^{-1}))+
+  geom_hline(yintercept = mean(SIFL$abond_std), col = "red", lty = 2)+
+  theme_classic()+
+  theme(axis.text.x = element_text(size = 10, angle = 45, vjust = 0.8))
+SIFL_plot
 
 
+##### TAPI
 
+TAPI_Ni <- TAPI$abond_std
+TAPI_P <- mean(TAPI$abond_std)
 
+TAPI_Nipi <- TAPI_Ni - TAPI_P # Numérateur
 
+TAPI_sigma <- sd(TAPI_Nipi) # Sigma
 
+TAPI_Di <- TAPI_Nipi/TAPI_sigma # Standard deviate
 
+TAPI_seuil <- abs(min(TAPI_Di)) # Seuil fixé pour irruption
 
+TAPI_irr <- TAPI_Di > TAPI_seuil # Vecteur logique TRUE or FALSE pour irruption
 
+# Save object to load in data_clean.R
+# save(TAPI_irr, file = "TAPI_irr.R")
 
-# Faire du VECTEUR IFELSE_IRR (1,0) un objet.R
-# Load object.R in data_clean.R
-# cbind(IFELSE_IRR)
-# write.csv in wd
+# Représentation graphique
 
-
-
+TAPI_plot <- ggplot(TAPI, aes(x = Annee, y = abond_std, color = irruption))+
+  scale_color_manual(values = c("1" = "violetred", "0" = "black"))+
+  geom_point(size = 4)+
+  geom_path(linewidth = 1, col = "#a7a7a7")+
+  labs(title = "Abondance standardisée annuelle du Tarin des pins pour les années irruptives VS non-irruptives",
+       x = "Année",
+       y = expression("N. individus recensés *" ~ heure^{-1}))+
+  geom_hline(yintercept = mean(TAPI$abond_std), col = "red", lty = 2)+
+  theme_classic()+
+  theme(axis.text.x = element_text(size = 10, angle = 45, vjust = 0.8))
+TAPI_plot
 
 
 
